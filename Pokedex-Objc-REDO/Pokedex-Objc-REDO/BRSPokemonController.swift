89b6a32e26ffdac8c12b404e5de9758dd6288d65
brysonS <brys1saclaus2@gmail.com>
2021-02-28 23:15:22 -0800

    Get pokemon details network call

--- Pokedex-Objc-REDO/Pokedex-Objc-REDO/BRSPokemonController.swift
+++ Pokedex-Objc-REDO/Pokedex-Objc-REDO/BRSPokemonController.swift
@@ -22,6 +22,7 @@ class BRSPokemonController: NSObject {
     private let fetchPokemonURL = URL(string: "https://pokeapi.co/api/v2/pokemon/?limit=1000")!
     private let fetchPokemonDetails = URL(string: "https://pokeapi.co/api/v2/pokemon/")!
     
+    //MARK: - Network Calls
     @objc func fetchAllPokemon(completion: @escaping ([BRSPokemon]?, Error?) -> Void) {
         var request = URLRequest(url: fetchPokemonURL)
         request.httpMethod = "GET"
@@ -57,6 +58,48 @@ class BRSPokemonController: NSObject {
                 completion(self.pokemon, nil)
             }
         }.resume()
+    }
+    
+    func getPokemonDetails(for pokemon: BRSPokemon) {
+        var request = URLRequest(url: fetchPokemonDetails.appendingPathComponent(pokemon.name.lowercased()))
+        request.httpMethod = "GET"
+        print(request)
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            guard let data = data else { return }
+            var topLevelDictionary : [String:Any]?
+            
+            do {
+                topLevelDictionary  = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any]
+            } catch let err as NSError {
+                print(err)
+                return
+            }
+            
+            guard let topLevelDict = topLevelDictionary else { return }
+            
+            // Get ID
+            guard let id = topLevelDict["id"] as? NSNumber else { return }
+            pokemon.identifier = id
+            
+            // Get Sprite
+            let spritesDictionary = topLevelDict["sprites"] as! [String:Any]
+            let imageURLString = spritesDictionary["front_default"] as? String
+            if let imageURL = URL(string: imageURLString!) {
+                guard let data = try? Data(contentsOf: imageURL) else { return }
+                pokemon.sprite = UIImage(data: data)!
+            }
+            // Get Abilities
+            let abilitiesArray = topLevelDict["abilities"] as! [[String:Any]]
+            for item in abilitiesArray {
+                let ability = item["ability"] as! [String:String]
+                let abi = ability["name"]!
+                pokemon.abilities.append(abi)
+            }
+        }.resume()
+        
+        
     }
     
     
